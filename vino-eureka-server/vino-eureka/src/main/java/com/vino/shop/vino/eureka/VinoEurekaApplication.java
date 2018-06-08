b2800f7e0cee9593c6aaa1b1ae4caaf2f8ecb251
vinohsueh <413311010@qq.com>
2018-06-08 15:09:44 +0800

    1

--- vino-eureka-server/vino-eureka/src/main/java/com/vino/shop/vino/eureka/VinoEurekaApplication.java
+++ vino-eureka-server/vino-eureka/src/main/java/com/vino/shop/vino/eureka/VinoEurekaApplication.java
@@ -1,4 +1,4 @@
-package com.vino.shop.vinoeureka;
+package com.vino.shop.vino.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
